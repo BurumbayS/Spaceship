e29ab5a86d52a9256bf869bbf7c8c4af0e72a315
BurumbayS <sanzharbr@gmail.com>
2016-03-06 11:03:53 +0600

    init

---
+++ GDI+/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace GDI_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(669, 324);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Paint += new System.Windows.Forms.PaintEventHandler(this.paint);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Rockwell", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBox1.Location = new System.Drawing.Point(524, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(118, 43);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Level : 1 Score : 200 Live: *** ";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(669, 324);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ GDI+/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GDI_
+{
+    public partial class Form1 : Form
+    {
+        Brush p = new SolidBrush(Color.Blue);
+        Brush pp = new SolidBrush(Color.Black);
+        Brush b = new SolidBrush(Color.White);
+        //Pen pen = new Pen(Color.Orange, (float)2.0);
+        Brush pen = new SolidBrush(Color.Orange);
+        Brush penn = new SolidBrush(Color.Blue);
+        Brush pe = new SolidBrush(Color.Red);
+        Brush pee = new SolidBrush(Color.Green);
+
+        // Point point1 = new Point(50, 50);
+        //Point point2 = new Point(100, 25);
+        //Point point3 = new Point(200, 5);
+        //Point point4 = new Point(250, 50);
+        //Point point5 = new Point(300, 100);
+        //Point point6 = new Point(350, 200);
+        //Point point7 = new Point(250, 250);
+        List<Point> curvePoints = new List<Point>();
+        List<Point> Arrow = new List<Point>();
+        List<Point> Asteroids = new List<Point>();
+        List<Point> Bullet = new List<Point>();
+
+        float x1 = 10;
+        float y1 = 10;
+        float x2 = 648;
+        float y2 = 300;
+
+        float xx1 = 0;
+        float yy1 = 0;
+        float xx2 = 668;
+        float yy2 = 330;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            curvePoints.Add(new Point(50, 170));
+            curvePoints.Add(new Point(100, 140));
+            curvePoints.Add(new Point(150, 170));
+            curvePoints.Add(new Point(150, 200));
+            curvePoints.Add(new Point(100, 230));
+            curvePoints.Add(new Point(50, 200));
+
+            Arrow.Add(new Point(100, 150));
+            Arrow.Add(new Point(110, 160));
+            Arrow.Add(new Point(105, 160));
+            Arrow.Add(new Point(105, 210));
+            Arrow.Add(new Point(95, 210));
+            Arrow.Add(new Point(95, 160));
+            Arrow.Add(new Point(90, 160));
+
+            Asteroids.Add(new Point(320, 100));
+            Asteroids.Add(new Point(330, 120));
+            Asteroids.Add(new Point(350, 123));
+            Asteroids.Add(new Point(335, 133));
+            Asteroids.Add(new Point(340, 153));
+            Asteroids.Add(new Point(320, 140));
+            Asteroids.Add(new Point(300, 153));
+            Asteroids.Add(new Point(305, 133));
+            Asteroids.Add(new Point(290, 123));
+            Asteroids.Add(new Point(310, 120));
+
+            Bullet.Add(new Point(100, 110));
+            Bullet.Add(new Point(105, 120));
+            Bullet.Add(new Point(115, 125));
+            Bullet.Add(new Point(105, 130));
+            Bullet.Add(new Point(100, 140));
+            Bullet.Add(new Point(95, 130));
+            Bullet.Add(new Point(85, 125));
+            Bullet.Add(new Point(95, 120));
+            // curvePoints.Add(new Point(250, 250));
+        }
+
+        private void paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias; e.Graphics.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            e.Graphics.FillRectangle(pp, xx1, yy1, xx2, yy2);
+            e.Graphics.FillRectangle(p,x1,y1,x2,y2);
+            e.Graphics.FillEllipse(b, 50, 50, 25 , 25);
+            e.Graphics.FillEllipse(b, 150, 60, 25, 25);
+            e.Graphics.FillEllipse(b, 300, 210, 25, 25);
+            e.Graphics.FillEllipse(b, 515, 120, 25, 25);
+            e.Graphics.FillEllipse(b, 420, 75, 25, 25);
+            e.Graphics.FillPolygon(pen, curvePoints.ToArray());
+            e.Graphics.FillPolygon(penn, Arrow.ToArray());
+            e.Graphics.FillPolygon(pe, Asteroids.ToArray());
+            e.Graphics.FillPolygon(pee, Bullet.ToArray());
+        }
+    }
+}
